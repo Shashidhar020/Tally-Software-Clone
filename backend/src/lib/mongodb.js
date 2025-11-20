603b41973171cbb5c1447ca65e38f53501ba158f
Shashidhar020 <shashidharab736@gmail.com>
2025-11-20 19:50:21 +0530

    api changed

--- backend/src/lib/mongodb.js
+++ backend/src/lib/mongodb.js
@@ -1,20 +1,23 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-const options = {
-  
-  retryWrites: true,
-};
+
 if (!uri) {
-  throw new Error("Please define MONGODB_URI in .env.local");
+  throw new Error("Please define MONGODB_URI in Vercel");
 }
+
 let client = global._mongoClient;
 let clientPromise;
+
 if (!client) {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri, {
+    tls: true,        // <-- Required for Atlas
+    retryWrites: true,
+  });
   global._mongoClient = client;
   clientPromise = client.connect();
 } else {
   clientPromise = Promise.resolve(client);
 }
+
 export default clientPromise;
