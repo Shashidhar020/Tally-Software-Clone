33140158bcf89a2922c8fc3e1e721657944c2e5c
Shashidhar020 <shashidharab736@gmail.com>
2025-11-21 19:17:46 +0530

    updated all folder inside src

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    appDir: true,
+  },
+  // Tell Next.js that your source code is inside the /src folder
+  srcDir: 'src',
 };
 
 export default nextConfig;
